0b0b6ed7a5c4f81ccc9e532ce5b2dac03300caff
abdulhamid Hayredin <abdulhamidhayredin94@gmail.com>
2022-09-08 14:08:16 -0700

    task done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
-/*
- * main: entry point 
+/**
+ * main : entry point
  *
- * print string
- * return 0: Success
+ * return 0 : Success
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
