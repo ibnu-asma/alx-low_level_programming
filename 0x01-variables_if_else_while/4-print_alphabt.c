e32246997d1b256d15781ddda1f8c5218f09de59
abdulhamid Hayredin <abdulhamidhayredin94@gmail.com>
2022-09-09 08:24:16 -0700

    task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 97;
+
+	for (; i <= 122; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+
+
+	putchar('\n');
+	return (0);
+}
